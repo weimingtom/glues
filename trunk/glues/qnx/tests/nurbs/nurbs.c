6feba3444db34d27112681c3844c5129707edb46
mike@malva.ua <mike@malva.ua@098db258-220c-11de-ac9f-1d9250355bca>
2009-09-14 18:16:46 +0000

    Added missing test skeletone for NURBS

---
+++ trunk/glues/qnx/tests/nurbs/nurbs.c
@@ -0,0 +1,180 @@
+/*                                                              */
+/* This test is based on tess.c from OpenGL Redbook,            */
+/* initialization part on QSSL's egl* demo                      */
+/*                                                              */
+/* // Mike Gorchak, 2009. GLU ES test                           */
+/*                                                              */
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include <gf/gf.h>
+#include <gf/gf3d.h>
+#include <GLES/gl.h>
+#include <GLES/egl.h>
+
+#include "glues.h"
+
+gf_dev_t    gfdev;
+gf_layer_t  layer;
+int         layer_idx;
+
+static EGLDisplay display;
+static EGLSurface surface;
+
+static EGLint attribute_list[]=
+{
+   EGL_NATIVE_VISUAL_ID, 0,
+   EGL_NATIVE_RENDERABLE, EGL_TRUE,
+   EGL_RED_SIZE, 5,
+   EGL_GREEN_SIZE, 5,
+   EGL_BLUE_SIZE, 5,
+   EGL_DEPTH_SIZE, 16,
+   EGL_NONE
+};
+
+void init_scene(int width, int height)
+{
+}
+
+void render_scene()
+{
+   /* Flush all drawings */
+   glFlush();
+}
+
+int main(int argc, char** argv)
+{
+   gf_3d_target_t      target;
+   gf_display_t        gf_disp;
+   EGLConfig           config;
+   EGLContext          econtext;
+   EGLint              num_config;
+   gf_dev_info_t       info;
+   gf_layer_info_t     linfo;
+   gf_display_info_t   disp_info;
+   GLuint              width, height;
+   GLuint              it;
+
+   /* initialize the graphics device */
+   if (gf_dev_attach(&gfdev, NULL, &info)!=GF_ERR_OK)
+   {
+      perror("gf_dev_attach()");
+      return -1;
+   }
+
+   /* Setup the layer we will use */
+   if (gf_display_attach(&gf_disp, gfdev, 0, &disp_info)!=GF_ERR_OK)
+   {
+      fprintf(stderr, "gf_display_attach() failed\n");
+      return -1;
+   }
+
+   width=disp_info.xres;
+   height=disp_info.yres;
+
+   layer_idx=disp_info.main_layer_index;
+
+   /* get an EGL display connection */
+   display=eglGetDisplay(gfdev);
+   if (display==EGL_NO_DISPLAY)
+   {
+      fprintf(stderr, "eglGetDisplay() failed\n");
+      return -1;
+   }
+
+   if (gf_layer_attach(&layer, gf_disp, layer_idx, 0)!=GF_ERR_OK)
+   {
+      fprintf(stderr, "gf_layer_attach() failed\n");
+      return -1;
+   }
+
+   /* initialize the EGL display connection */
+   if (eglInitialize(display, NULL, NULL)!=EGL_TRUE)
+   {
+      fprintf(stderr, "eglInitialize: error 0x%x\n", eglGetError());
+      return -1;
+   }
+
+   for (it=0;; it++)
+   {
+      /* Walk through all possible pixel formats for this layer */
+      if (gf_layer_query(layer, it, &linfo)==-1)
+      {
+         fprintf(stderr, "Couldn't find a compatible frame "
+                         "buffer configuration on layer %d\n", layer_idx);
+         return -1;
+      }
+
+      /*
+       * We want the color buffer format to match the layer format,
+       * so request the layer format through EGL_NATIVE_VISUAL_ID.
+       */
+      attribute_list[1]=linfo.format;
+
+      /* Look for a compatible EGL frame buffer configuration */
+      if (eglChooseConfig(display, attribute_list, &config, 1, &num_config)==EGL_TRUE)
+      {
+         if (num_config>0)
+         {
+            break;
+         }
+      }
+   }
+
+   /* create a 3D rendering target */
+   if (gf_3d_target_create(&target, layer, NULL, 0, width, height, linfo.format)!=GF_ERR_OK)
+   {
+      fprintf(stderr, "Unable to create rendering target\n");
+      return -1;
+   }
+
+   gf_layer_set_src_viewport(layer, 0, 0, width-1, height-1);
+   gf_layer_set_dst_viewport(layer, 0, 0, width-1, height-1);
+   gf_layer_enable(layer);
+
+   /*
+    * The layer settings haven't taken effect yet since we haven't
+    * called gf_layer_update() yet.  This is exactly what we want,
+    * since we haven't supplied a valid surface to display yet.
+    * Later, the OpenGL ES library calls will call gf_layer_update()
+    * internally, when  displaying the rendered 3D content.
+    */
+
+   /* create an EGL rendering context */
+   econtext=eglCreateContext(display, config, EGL_NO_CONTEXT, NULL);
+   if (econtext==EGL_NO_CONTEXT)
+   {
+      fprintf(stderr, "Create context failed: 0x%x\n", eglGetError());
+      return -1;
+   }
+
+   /* create an EGL window surface */
+   surface=eglCreateWindowSurface(display, config, target, NULL);
+   if (surface==EGL_NO_SURFACE)
+   {
+      fprintf(stderr, "Create surface failed: 0x%x\n", eglGetError());
+      return -1;
+   }
+
+   /* connect the context to the surface */
+   if (eglMakeCurrent(display, surface, surface, econtext)==EGL_FALSE)
+   {
+      fprintf(stderr, "Make current failed: 0x%x\n", eglGetError());
+      return -1;
+   }
+
+//   eglSwapInterval(display, 0);
+
+   init_scene(width, height);
+
+   do {
+      render_scene();
+      glFinish();
+      eglWaitGL();
+      eglSwapBuffers(display, surface);
+   } while(1);
+
+   return 0;
+}
